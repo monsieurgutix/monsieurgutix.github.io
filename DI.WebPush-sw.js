b74bf6746057cc44031f5104f2ab26e1bbfc8886
Iván Delgado <idelgado@dialoginsight.com>
2018-09-12 11:34:28 -0400

    Changes

--- DI.WebPush-sw.js
+++ DI.WebPush-sw.js
@@ -1,1 +1,1 @@
-importScripts('https://devapp.hq2.rep/T/OFC4/WPS/1127/8287/0xCYGJgoz4KusZ0wS6UkdUdpYBYTVgyZ/DI.WebPush-sw-lib.js');
+importScripts('https://lightspeed.dev.ofsys.com/T/OFC4/WPS/1127/8287/0xCYGJgoz4KusZ0wS6UkdUdpYBYTVgyZ/DI.WebPush-sw-lib.js');
